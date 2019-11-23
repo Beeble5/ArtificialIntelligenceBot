a3b6151674898ba1e66df1d01430d5b0385dd629
Loyola User <Confer18@luc.edu>
2019-11-23 11:57:27 -0600

    Added ability to add jokes and feelings

--- bot.py
+++ bot.py
@@ -9,19 +9,44 @@ jokes = ["Did you hear about the mathmatician who's afraid of negative numbers? 
          "Calibri and Times New Roman walk into a resturant. \"Get out of here!\" shouts the waiter. \"We don\'t serve your type!\"",
          "A bear walks into a snack shop and says, \“Give me a juice box and … milk.\” \“Why the big pause?\” asks the bartender. The bear shrugged. \“I\’m not sure; I was born with them.\”"]
 
+
 while True:
+    user_input.lower() = input("Enter something: ")
     if user_input.lower() == 'hi' or user_input.lower() == 'hello':
         print("Hello, how are you?")
     elif user_input.lower() == 'how are you?' or user_input.lower() == 'hello, how are you?':
         print("I'm feeling " + random.choice(feelings) + random.choice(feelingQuestions))
     elif user_input.lower() == 'terrible' or user_input.lower() == 'bad' or user_input.lower() == 'tired' or user_input.lower() == 'worn out':
         print("I\'m sorry. Sounds like you've had a rough day. Would it cheer you up if I told you a joke?")
-        user_input = input("Type something: ")
+        user_input.lower() = input("Type something: ")
         if user_input.lower() == 'yes':
             print(random.choice(jokes))
+    elif user_input.lower() == "good" or user_input.lower() == "super" or user_input.lower() == "great" or user_input.lower() == "fine":
+            print("That's good")
+    elif user_input.lower() == "Let's do some math" or user_input.lower() == "math":
+        x = float(input("Enter a number: "))
+        y = float(input("Enter a number: "))
+        print("What operation?")
+        operation = input("Type an operation: ")
+        if operation == "Multiplication":
+            print(x * y)
+        elif operation == "Division":
+            print(x / y)
+        elif operation == "Addition":
+            print(x + y)
+        elif operation == "Subtraction":
+            print(x - y)
+    elif user_input.lower() == "Add feelings":
+        feel = input("Enter a feeling: ")
+        feelings.append(feel)
+    elif user_input.lower() == "Add jokes" or "Add joke":
+        joke = input("Enter a joke: ")
+        jokes.append(joke)
+    elif user_input.lower() == "Could you tell me a joke?":
+        print(random.choice(jokes))
     elif user_input.lower() == 'stop':
          break
     else:
-        print(random.choice(answers))
+        print(answers)
     
 
