406dc3c210dbe6cbf7e7f9fd3c1538400a752b1c
M Bush <mjb2306b@gmail.com>
2019-12-06 18:13:13 -0600

    Attempted to add a dictionary

--- bot.py
+++ bot.py
@@ -4,25 +4,27 @@ answers = ['I\'m afraid I don\'t understand what you just said']
 feelings = ['bad.', 'good.', 'great!', 'super!', 'terrible. :(', 'okay.', 'fine.']
 feelingQuestions = [' How are you?', ' How about you?']
 userWorseFeelings = ['Terrible', 'Bad', 'Tired', 'Worn out', ]
-jokes = ["Did you hear about the mathmatician who's afraid of negative numbers? They'll stop at nothing to avoid them.",
+jokes = ["Did you hear about the mathematician who's afraid of negative numbers? They'll stop at nothing to avoid them.",
          "Why do we tell actors to \"break a leg\"? Because every play (or musical) has a cast.",
-         "Calibri and Times New Roman walk into a resturant. \"Get out of here!\" shouts the waiter. \"We don\'t serve your type!\"",
+         "Calibri and Times New Roman walk into a restaurant. \"Get out of here!\" shouts the waiter. \"We don\'t serve your type!\"",
          "A bear walks into a snack shop and says, \“Give me a juice box and … milk.\” \“Why the big pause?\” asks the bartender. The bear shrugged. \“I\’m not sure; I was born with them.\”"]
-
+d = {}
 
 while True:
-    user_input.lower() = input("Enter something: ")
+    user_input = input("Enter something: ")
     if user_input.lower() == 'hi' or user_input.lower() == 'hello':
         print("Hello, how are you?")
     elif user_input.lower() == 'how are you?' or user_input.lower() == 'hello, how are you?':
         print("I'm feeling " + random.choice(feelings) + random.choice(feelingQuestions))
-    elif user_input.lower() == 'terrible' or user_input.lower() == 'bad' or user_input.lower() == 'tired' or user_input.lower() == 'worn out':
+    elif user_input.lower() == 'terrible' or user_input.lower() == 'bad' or user_input.lower() == 'tired' or \
+            user_input.lower() == 'worn out':
         print("I\'m sorry. Sounds like you've had a rough day. Would it cheer you up if I told you a joke?")
-        user_input.lower() = input("Type something: ")
+        user_input = input("Type something: ")
         if user_input.lower() == 'yes':
             print(random.choice(jokes))
-    elif user_input.lower() == "good" or user_input.lower() == "super" or user_input.lower() == "great" or user_input.lower() == "fine":
-            print("That's good")
+    elif user_input.lower() == "good" or user_input.lower() == "super" or user_input.lower() == "great" or \
+            user_input.lower() == "fine":
+        print("That's good")
     elif user_input.lower() == "Let's do some math" or user_input.lower() == "math":
         x = float(input("Enter a number: "))
         y = float(input("Enter a number: "))
@@ -39,14 +41,21 @@ while True:
     elif user_input.lower() == "Add feelings":
         feel = input("Enter a feeling: ")
         feelings.append(feel)
-    elif user_input.lower() == "Add jokes" or "Add joke":
-        joke = input("Enter a joke: ")
-        jokes.append(joke)
+    #elif user_input.lower() == "Add jokes" or "Add joke":
+        #joke = input("Enter a joke: ")
+        #jokes.append(joke)
     elif user_input.lower() == "Could you tell me a joke?":
         print(random.choice(jokes))
+    elif user_input.lower() == "add value":
+        key = input("Enter a key: ")
+        val = input("Enter a value: ")
+        x = input("Enter the key of the value you are trying to retrieve: ")
+        d[key] = val
+        if x == key:
+            print(val)
     elif user_input.lower() == 'stop':
-         break
+        break
     else:
         print(answers)
-    
+
 
