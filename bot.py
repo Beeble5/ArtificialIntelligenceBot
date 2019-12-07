98811cdc782657f16dfacc3f5b144586f99fadb9
Loyola User <Confer18@luc.edu>
2019-12-07 11:06:17 -0600

    Continued with dictionary

--- bot.py
+++ bot.py
@@ -1,16 +1,20 @@
 import random
+import json
 
 answers = ['I\'m afraid I don\'t understand what you just said']
 feelings = ['bad.', 'good.', 'great!', 'super!', 'terrible. :(', 'okay.', 'fine.']
 feelingQuestions = [' How are you?', ' How about you?']
 userWorseFeelings = ['Terrible', 'Bad', 'Tired', 'Worn out', ]
-jokes = ["Did you hear about the mathematician who's afraid of negative numbers? They'll stop at nothing to avoid them.",
+jokes = ["Did you hear about the mathematician who's afraid of"
+         "negative numbers? They'll stop at nothing to avoid them.",
          "Why do we tell actors to \"break a leg\"? Because every play (or musical) has a cast.",
          "Calibri and Times New Roman walk into a restaurant. \"Get out of here!\" shouts the waiter. \"We don\'t serve your type!\"",
          "A bear walks into a snack shop and says, \“Give me a juice box and … milk.\” \“Why the big pause?\” asks the bartender. The bear shrugged. \“I\’m not sure; I was born with them.\”"]
 d = {}
 
 while True:
+    print("My dictionary: {}".format(d))
+
     user_input = input("Enter something: ")
     if user_input.lower() == 'hi' or user_input.lower() == 'hello':
         print("Hello, how are you?")
@@ -49,13 +53,18 @@ while True:
     elif user_input.lower() == "add value":
         key = input("Enter a key: ")
         val = input("Enter a value: ")
-        x = input("Enter the key of the value you are trying to retrieve: ")
         d[key] = val
-        if x == key:
-            print(val)
+    elif user_input.lower() == "print value":
+        x = input("Enter the key of the value you are trying to retrieve: ")
+        print(d.get(x, "This key does not exist!"))
+        #while user_input.lower() != "end":
+         #   if x == key:
+         #       print(val)
+         #       x = input("Enter the key of the value you are trying to retrieve: ")
+         #       #continue
+         #   elif x == "end":
+         #       break
     elif user_input.lower() == 'stop':
         break
     else:
         print(answers)
-
-
